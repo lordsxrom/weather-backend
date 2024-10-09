2360d05d8826cfb5478a42735f5ccbdce4392186
shumskii.n <lordsxrom@gmail.com>
2024-10-09 23:29:58 +0300

    Init

---
+++ src/test/kotlin/ru/shumskii/weather/WeatherApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.shumskii.weather
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
